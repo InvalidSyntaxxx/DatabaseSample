be1f0fa86bbbae86b62b67f08c455601f49c0b0d
Redamancy9189 <1791081494@qq.com>
2022-09-18 17:54:05 +0800

    添加表格硬编码：在insert statement中使用

--- main.c
+++ main.c
@@ -5,18 +5,34 @@
  * @Author: 王远昭 <email: wangyuanzhao35@gmail.com>
  * @Date: 2022-09-12 13:07:55
  * @LastEditors: 王远昭
- * @LastEditTime: 2022-09-12 22:20:36
+ * @LastEditTime: 2022-09-18 17:52:51
  */
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
 #include<string.h>
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+typedef unsigned __int32 uint32_t;
 
-typedef enum{ META_COMMAND_SUCCESS,META_COMMAND_UNRECOGNIZED_COMMAND} MetaCommandResult; //元命令数据结果，成功或无法识别
+/****************************************************************************************************************/
+typedef enum{ META_COMMAND_SUCCESS, META_COMMAND_UNRECOGNIZED_COMMAND} MetaCommandResult; //元命令数据结果，成功或无法识别
 
-typedef enum { PREPARE_SUCCESS,PREPARE_UNRECOGNIZED_STATEMENT} PrepareResult; // SQL 执行结果，成功或无法识别
+typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT, PREPARE_SYNTAX_ERROR} PrepareResult; // SQL 执行结果，成功或无法识别
 
-typedef enum{ STATEMENT_INSERT,STATEMENT_SELECT } StatementType; // SQL 语句类别
+typedef enum{ STATEMENT_INSERT, STATEMENT_SELECT } StatementType; // SQL 语句类别
+
+
+/**
+ * @brief 列数据硬编码(Hart Code)
+ * 
+ */
+typedef struct
+{
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
 
 /**
  * @brief 输入缓冲流
@@ -36,6 +52,7 @@ typedef struct
 typedef struct 
 {
     StatementType type;
+    Row row_to_insert;      // 只能在 insert statement 中使用
 } Statement;
 
 /**
@@ -113,6 +130,13 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer){
 PrepareResult prepare_statement(InputBuffer* input_buffer,Statement* statement){
     if(strncmp(input_buffer->buffer,"insert\n",6) == 0){
         statement->type = STATEMENT_INSERT;
+        int args_assigned = sscanf(
+            input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
+            statement->row_to_insert.username, statement->row_to_insert.email
+        );
+        if(args_assigned < 3){
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     if(strcmp(input_buffer->buffer,"select\n") == 0){
@@ -142,7 +166,7 @@ void execute_statement(Statement* statement){
 }
 
 /**
- * @brief 主函数
+ * @brief 主函数，实现简单的 REPL(Read-Eval-Print-Loop)交互式编程环境 
  * 
  * @param argc 
  * @param argv 
